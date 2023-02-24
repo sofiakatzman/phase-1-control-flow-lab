4ac7901620910fd1a1201df4f72047fb75c04407
Sofia Katzman <katzmansof@gmail.com>
2023-02-24 15:00:17 -0500

    Finished Lab!

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+function scuberGreetingForFeet(distance){
+  if (distance < 400) {
+    return 'This one is on me!'
+}
+  if (distance > 400 && distance < 2000) {
+    return 'That will be twenty bucks.'
+}
+  if (distance > 2000 && distance < 2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+else {
+  return 'No can do.'
+}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(cityDestination){
+  return cityDestination == 'NYC' ? 'Ok, sounds good.' : 'No go.';
+ 
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+switch (tip) {
+  case 'generous':
+    return 'Thank you so much.';
+  case 'not as generous':
+    return 'Thank you.';  
+  default:
+    return 'Bye.';  
+}
+
 }
